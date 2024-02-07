a025e0e6753ea95ea6313682eb617469b3e64bda
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-02-07 23:35:43 +0000

    Initial Commit

---
+++ src/views/Register.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import AppContext from "../providers/AppContext";
+
+const Register = () => {
+  const { setConext } = useContext(AppContext);
+
+  const register = () => {
+    
+  }
+};
+
+export default Register;
